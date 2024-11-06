a97a4a5e381e548e66e85bd93ed7a9a4392001f6
Zeus <137693574+Snake1216@users.noreply.github.com>
2024-11-06 19:17:34 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -230,8 +230,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Deepsea"
+        subTitle="test"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
